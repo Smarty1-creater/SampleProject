8b8f7913abd082f4d528fa292d768675ba7a3855
Smarty1-creater <128989990+Smarty1-creater@users.noreply.github.com>
2024-06-22 09:38:37 +0530

    Update propertyRepository.java

--- project-root/src/main/java/com/example/repository/propertyRepository.java
+++ project-root/src/main/java/com/example/repository/propertyRepository.java
@@ -1,9 +1,9 @@
 package com.example.repository;
 
-import com.example.model.Property;
+import com.example.model.property;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface propertyRepository extends JpaRepository<Property, Long> {
+public interface propertyRepository extends JpaRepository<property, Long> {
 }
